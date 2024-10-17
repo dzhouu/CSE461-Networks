d980872c9a3e7c85a18d1dc69089da085c1bfbe5
Denny Zhou <dgzhou11@gmail.com>
2024-10-16 23:00:56 -0700

    Added make response for server

--- lab1/server.py
+++ lab1/server.py
@@ -7,7 +7,6 @@ import random
 
 CLIENT_TIMEOUT = 3
 
-
 def make_header(payload_len, step, id, psecret):
     header = bytes()
     header += payload_len.to_bytes(4, 'big')
@@ -16,6 +15,14 @@ def make_header(payload_len, step, id, psecret):
     header += id.to_bytes(2, 'big')
     return header
 
+def make_response(num, len, udp_port, secretA):
+    response = bytes()
+    response += num.to_bytes(4, 'big')
+    response += len.to_bytes(4, 'big')
+    response += udp_port.to_bytes(4, 'big')
+    response += secretA.to_bytes(4, 'big')
+
+    return response
 """
 Parameters:
 header: must be a 12-byte object
@@ -34,6 +41,7 @@ def verify_header(header, exp_secret, exp_step, exp_id):
     _, header_secret, header_step, header_id = read_header(header)
     return (header_secret == exp_secret and header_step == exp_step and header_id == exp_id)
 
+<<<<<<< Updated upstream
 """
 Parameters:
 data: list of (data, num_bytes) to package into a payload
@@ -52,11 +60,19 @@ def handle_new_connection(socket, student_id):
     try:
         # Part a
         # Check for correct header 
+=======
+def partA(student_id ):
+    # Part a
+    try:
+>>>>>>> Stashed changes
         header = socket.recv(12)
         if len(header) != 12:
             print("I think this checks for the first 12 bytes for header?")
             return
 
+        client_message = "hello world" + "\0"
+        client_message_len = header + len(client_message)
+        # Check for correct header
         payload_len, secret, step, student_id, payload = read_header(header)
 
         if not verify_header(header, 0, 1, student_id):
@@ -69,14 +85,27 @@ def handle_new_connection(socket, student_id):
             return
 
         # Make random data for part A response
+<<<<<<< Updated upstream
         a_num, a_len, a_udp_port, secretA = random.randint(7, 20), random.randint(20, 100), random.randint(0, 65535), random.randint(100, 999)
         
         response_header = make_header(len(response_payload), 0, 2, student_id)
         response_payload = package_payload([(a_num, 4), (a_len, 4), (a_udp_port, 4), (secretA, 4)])
-        socket.send(response_header + response_payload)
+=======
+        a_num, a_len, a_udp_port, secretA = random.randint(7, 20), random.randint(20, 100), random.randint(10000,
+                                                                                                           65535), random.randint(
+            100, 999)
 
+        response_header = make_header(len(response_payload), 0, 2, student_id)
+        response_payload = make_response(a_num, a_len, a_udp_port, secretA)
+        socket.settimeout(CLIENT_TIMEOUT)
+>>>>>>> Stashed changes
+        socket.send(response_header + response_payload)
     except:
         pass
+
+
+def handle_new_connection(socket, student_id):
+    partA()
 
 
 def start_server(server_ip, port, student_id):
@@ -98,9 +127,17 @@ def start_server(server_ip, port, student_id):
         finally:
             server.close()
 
+if __name__ == '__main__':
+    server_ip = "attu2.cs.washington.edu"
+    port = 31415
+    id = 783
+    start_server(server_ip, port, id)
 
+<<<<<<< Updated upstream
 # Run this to start the server
 server_ip = "attu2.cs.washington.edu"
 port = 31415
 id = 783
 start_server(server_ip, port, id)
+=======
+>>>>>>> Stashed changes
